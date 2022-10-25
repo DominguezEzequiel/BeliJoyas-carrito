5f08809fc59a9d0c3dd4e0075d588c84a670a876
Ezequiel Dominguez <Dominguezezequiel97@gmail.com>
2022-10-25 20:09:48 -0300

    Tercera pre entrega del proyecto

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let modalCarrito = document.getElementById("carritoModal");
 let cantidadCarrito = document.getElementById("cantidadCarrito");
 
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 /* Recorro el array productos para agregar cada objeto a mi HTML */
 productos.forEach((product) => {
@@ -36,68 +36,27 @@ productos.forEach((product) => {
         });
         contadorCarrito();
         localSave();
+        verCarrito();
     });
 
 });
 
-/* Creo modal para ver el carrito */
-
-verCarrito.addEventListener("click", () => {
-
-    modalCarrito.innerHTML = ''
-
-    modalCarrito.style.display = "flex"
-    const carritoHeader = document.createElement("div");
-    carritoHeader.className = "carritoHeader";
-    carritoHeader.innerHTML = `
-    <h2 class="carrito-titulo">TUS COMPRAS</h2>
-    `
-    modalCarrito.append(carritoHeader);
-
-    /* Creo botón "X" para cerrar el carrito */
-
-    const cerrarCarrito = document.createElement("button");
-    cerrarCarrito.innerHTML = `<i class="fa-sharp fa-solid fa-circle-xmark"></i>`;
-    carritoHeader.append(cerrarCarrito);
-
-    cerrarCarrito.addEventListener("click", () => {
-        modalCarrito.style.display = "none"
-    })
-
-    /* Creo el cuerpo del modal, recorriendo el array carrito */
-
-    carrito.forEach((product) => {
-        let carritoBody = document.createElement("div")
-        carritoBody.className = "carritoLista"
-        carritoBody.innerHTML = `
-        <img src="${product.img}">
-        <p>${product.nombre}</p>
-        <p>$${product.precio}</p>
-    `
-        modalCarrito.append(carritoBody)
-    });
-
-    /* Creo el footer del modal con el metodo reduce para calcular el total */
-    const total = carrito.reduce((acc, e) => acc + e.precio, 0);
-
-    const carritoFooter = document.createElement("div")
-    carritoFooter.className = "carritoFooter";
-    carritoFooter.innerHTML = `
-    <h2>PRECIO FINAL:</h2> 
-    <h2> $${total}</h2>
-    `;
-    modalCarrito.append(carritoFooter);
-})
 
 /* Creo función para mostrar el contador de productos del carrito */
 
 const contadorCarrito = () => {
     cantidadCarrito.style.display = "block";
-    cantidadCarrito.innerText = carrito.length;
+
+    let carritoLength = carrito.length;
+
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
 };
 
-/* localstorage */
+contadorCarrito();
 
 const localSave = () => {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
+
