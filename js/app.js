2b5bf9c1055a554f1528a4a35c7c1bf1221e8b3b
Ezequiel Dominguez <Dominguezezequiel97@gmail.com>
2022-10-25 03:44:54 -0300

    crea modal carrito

--- js/app.js
+++ js/app.js
@@ -37,13 +37,49 @@ productos.forEach((product) => {
 
 /* Creo modal para ver el carrito */
 
-verCarrito.addEventListener("click", ()=> {
-    const carritoHeader = document.createElement("div");
-    carritoHeader.className="carritoHeader";
-    carritoHeader.innerHTML= `
-    <h2 class="carrito-titulo">Carrito de compras</h2>
-    <p>X</p>
-    `
-    modalCarrito.append(carritoHeader)
+verCarrito.addEventListener("click", () => {
 
+    modalCarrito.innerHTML = ''
+
+    modalCarrito.style.display = "flex"
+    const carritoHeader = document.createElement("div");
+    carritoHeader.className = "carritoHeader";
+    carritoHeader.innerHTML = `
+    <h2 class="carrito-titulo">TUS COMPRAS</h2>
+    `
+    modalCarrito.append(carritoHeader);
+
+    /* Creo botón "X" para cerrar el carrito */
+
+    const cerrarCarrito = document.createElement("button");
+    cerrarCarrito.innerHTML = `<i class="fa-sharp fa-solid fa-circle-xmark"></i>`;
+    carritoHeader.append(cerrarCarrito);
+
+    cerrarCarrito.addEventListener("click", () => {
+        modalCarrito.style.display = "none"
+    })
+
+    /* Creo el cuerpo del modal, recorriendo el array carrito */
+
+    carrito.forEach((product) => {
+        let carritoBody = document.createElement("div")
+        carritoBody.className = "carritoLista"
+        carritoBody.innerHTML = `
+        <img src="${product.img}">
+        <p>${product.nombre}</p>
+        <p>$${product.precio}</p>
+    `
+        modalCarrito.append(carritoBody)
+    });
+
+    /* Creo el footer del modal con el metodo reduce para calcular el total */
+    const total = carrito.reduce((acc, e) => acc + e.precio, 0);
+
+    const carritoFooter = document.createElement("div")
+    carritoFooter.className = "carritoFooter";
+    carritoFooter.innerHTML = `
+    <h2>PRECIO FINAL:</h2> 
+    <h2> $${total}</h2>
+    `;
+    modalCarrito.append(carritoFooter);
 })
